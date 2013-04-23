049dd5119b4beaacb760ae2321a6638ca9eed984
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-04-23 14:48:36 +0900

    fragment practice

--- practice/fundamentals/2nd/FragmentPractice/Practice1/src/jp/mixi/practice/fragment/beg/MainActivity.java
+++ practice/fundamentals/2nd/FragmentPractice/Practice1/src/jp/mixi/practice/fragment/beg/MainActivity.java
@@ -1,23 +1,26 @@
-
 package jp.mixi.practice.fragment.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		FragmentManager manager = getSupportFragmentManager();
+		Fragment fragment = manager.findFragmentById(R.id.fragment);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
