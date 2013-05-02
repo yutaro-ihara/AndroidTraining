499d05359ee58cc462333ed462bba0946d25403e
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-02 18:57:48 +0900

    submit assignment1

--- assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
@@ -1,30 +1,68 @@
-
 package jp.mixi.assignment.controller.beg;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 /**
- * TODO: 課題1
- * {@link Toast#makeText(Context, CharSequence, int)} または
+ * TODO: 課題1 {@link Toast#makeText(Context, CharSequence, int)} または
  * {@link Toast#makeText(Context, int, int)} を利用して、各ライフサイクルメソッドがどのような順番で
- * 実行されているかを確認してください。
- * 確認したら、assignments/fundamentals/2nd/Report.md にその順番を記述してください。
+ * 実行されているかを確認してください。 確認したら、assignments/fundamentals/2nd/Report.md
+ * にその順番を記述してください。
+ * 
  * @author keishin.yokomaku
  */
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		Toast.makeText(this, "create", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Toast.makeText(this, "resume", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Toast.makeText(this, "pause", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Toast.makeText(this, "stop", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "destroy", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Toast.makeText(this, "restart", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
--- assignments/fundamentals/2nd/Report.md
+++ assignments/fundamentals/2nd/Report.md
@@ -9,4 +9,30 @@ Activity と Fragment
 ### Activity
 
 3. ライフサイクルのコールバックメソッドの呼び出し順
+* 起動時
+    * create
+    * start
+    * resume
+* back button -> 起動
+    * pause
+    * stop
+    * destroy
+    * 再起動
+        * create
+        * start
+        * resume
+* home button -> 起動
+    * pause
+    * stop
+    * 再起動
+        * restart
+        * start
+        * resume
+* device orientation
+    * pause
+    * stop
+    * destroy
+    * create
+    * start
+    * resume
 
