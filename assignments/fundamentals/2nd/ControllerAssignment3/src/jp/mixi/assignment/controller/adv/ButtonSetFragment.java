29d60395ee89e61c9be9deb6c8117753875b3228
Yutaro IHARA <buimasa1019@gmail.com>
2013-05-07 02:28:47 +0900

    submit 2.02 - practice 7

---
+++ assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/ButtonSetFragment.java
@@ -0,0 +1,16 @@
+package jp.mixi.assignment.controller.adv;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ButtonSetFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_button_set, container, false);
+		return view;
+	}
+}
