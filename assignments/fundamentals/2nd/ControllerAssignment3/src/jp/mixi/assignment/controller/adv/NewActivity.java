4e0b9f8a82a1eb9e6c1880551dd580fe8d95dc11
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-07 19:28:35 +0900

    fix practice 7 and submit practice 8

---
+++ assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/NewActivity.java
@@ -0,0 +1,13 @@
+package jp.mixi.assignment.controller.adv;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class NewActivity extends FragmentActivity {
+	@Override
+	protected void onCreate(Bundle bundle) {
+		// TODO Auto-generated method stub
+		super.onCreate(bundle);
+        setContentView(R.layout.activity_new);
+	}
+}
