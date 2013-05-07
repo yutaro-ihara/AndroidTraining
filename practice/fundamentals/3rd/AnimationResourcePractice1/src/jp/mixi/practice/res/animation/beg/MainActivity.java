542690844a3cfd4f979ac242b1642bee2a17517d
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-07 21:28:48 +0900

    submit animation practice

--- practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
+++ practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
@@ -1,23 +1,40 @@
-
 package jp.mixi.practice.res.animation.beg;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		View textView = findViewById(R.id.text_view);
+		Animation animation = AnimationUtils.loadAnimation(
+				this, R.anim.animation);
+		textView.startAnimation(animation);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
