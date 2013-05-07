e2f7acb23dfb8c06bf914e3dee7ca8e58b104e64
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-07 18:44:33 +0900

    submit notification practice 1 and 2

--- practice/fundamentals/4th/NotificationPractice1/src/jp/mixi/practice/notification/beg/MainActivity.java
+++ practice/fundamentals/4th/NotificationPractice1/src/jp/mixi/practice/notification/beg/MainActivity.java
@@ -1,16 +1,52 @@
-
 package jp.mixi.practice.notification.beg;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        // TODO ここで通知を表示する
-    }
+		// TODO ここで通知を表示する
+		Context context = getApplicationContext();
+		Intent intent = new Intent(context, NotificationActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+				intent, 0);
+
+		NotificationManager manager = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+		Notification notification = new NotificationCompat.Builder(context)
+				.setAutoCancel(true).setTicker("hoge")
+				.setSmallIcon(R.drawable.ic_launcher).setContentTitle("hoge")
+				.setContentText(getString(R.string.notification_message))
+				.setContentIntent(contentIntent).build();
+		manager.notify(R.string.app_name, notification);
+		/*
+		 * Uri uri = Uri.parse(json.getString("url")); Intent remoteIntent = new
+		 * Intent(Intent.ACTION_VIEW, uri); PendingIntent contentIntent =
+		 * PendingIntent.getActivity( context.getApplicationContext(), 0,
+		 * remoteIntent, 0);
+		 * 
+		 * NotificationManager manager = (NotificationManager) context
+		 * .getSystemService(Context.NOTIFICATION_SERVICE);
+		 * 
+		 * Notification notification = new NotificationCompat.Builder(
+		 * context.getApplicationContext()).setAutoCancel(true)
+		 * .setTicker(msg.toString())
+		 * .setContentTitle(context.getString(R.string.app_name))
+		 * .setSmallIcon(R.drawable.ic_launcher) .setContentText(msg.toString())
+		 * .setContentIntent(contentIntent).build();
+		 * 
+		 * manager.notify(R.string.app_name, notification);
+		 */
+	}
 }
