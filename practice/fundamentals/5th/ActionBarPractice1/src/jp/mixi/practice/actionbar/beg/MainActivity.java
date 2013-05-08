a7f5576a7bdb5487a1bdd9e46de0141cd6265826
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-08 13:12:23 +0900

    submit actionbar practice1-3

--- practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
+++ practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
@@ -2,22 +2,41 @@
 package jp.mixi.practice.actionbar.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity implements
+TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ActionBar bar = getSupportActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        bar.addTab(bar.newTab().setText("tab1").setTabListener(this));
+        bar.addTab(bar.newTab().setText("tab2").setTabListener(this));
+        bar.addTab(bar.newTab().setText("tab3").setTabListener(this));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		Toast.makeText(getApplicationContext(), tab.getText(), Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		Toast.makeText(getApplicationContext(), tab.getText(), Toast.LENGTH_SHORT).show();
+	}
 
 }
