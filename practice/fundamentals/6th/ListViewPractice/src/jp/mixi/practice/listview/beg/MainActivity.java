0dffa3793aeb0160387ea6ea9e90ff7c457eaafc
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-13 13:43:41 +0900

    listview practice 2

--- practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
+++ practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
@@ -2,14 +2,15 @@ package jp.mixi.practice.listview.beg;
 
 import java.util.ArrayList;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private MainActivity mActivity;
@@ -19,15 +20,16 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mActivity = this;
-		
+
 		ArrayList<String> list = new ArrayList<String>();
 		for (int i = 0; i < 60; i++) {
 			list.add("hoge" + i);
 		}
-		
+
 		ListView listView = (ListView) findViewById(R.id.ListView);
-		CustomListItemAdapter adapter = new CustomListItemAdapter(mActivity, list);
-		
+		CustomListItemAdapter adapter = new CustomListItemAdapter(mActivity,
+				list);
+
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(new OnItemClickListener() {
 
@@ -37,7 +39,17 @@ public class MainActivity extends Activity {
 				String str = (String) arg0.getItemAtPosition(arg2);
 				Toast.makeText(mActivity, str, Toast.LENGTH_SHORT).show();
 			}
-			
+
+		});
+
+		View button = findViewById(R.id.Button);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				ListView listView = (ListView) findViewById(R.id.ListView);
+				listView.smoothScrollToPosition(0);
+			}
 		});
 	}
 
