83db52fc61344f14a72d6be33fff276273db4205
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-13 13:27:40 +0900

    list view practice 1

--- practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
+++ practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
@@ -1,23 +1,51 @@
-
 package jp.mixi.practice.listview.beg;
+
+import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	private MainActivity mActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mActivity = this;
+		
+		ArrayList<String> list = new ArrayList<String>();
+		for (int i = 0; i < 60; i++) {
+			list.add("hoge" + i);
+		}
+		
+		ListView listView = (ListView) findViewById(R.id.ListView);
+		CustomListItemAdapter adapter = new CustomListItemAdapter(mActivity, list);
+		
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				String str = (String) arg0.getItemAtPosition(arg2);
+				Toast.makeText(mActivity, str, Toast.LENGTH_SHORT).show();
+			}
+			
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
 }
