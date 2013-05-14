5bc57401c400a1feefbecdbe88275baa5a060635
yutaro.ihara <yutaro.ihara@mixi.co.jp>
2013-05-14 12:41:23 +0900

    sharedprefrence practice

--- practice/fundamentals/7th/SharedPreferencesPractice1/src/jp/mixi/practice/sharedpreferences/sharedpreferencespractice1/MainActivity.java
+++ practice/fundamentals/7th/SharedPreferencesPractice1/src/jp/mixi/practice/sharedpreferences/sharedpreferencespractice1/MainActivity.java
@@ -2,11 +2,13 @@ package jp.mixi.practice.sharedpreferences.sharedpreferencespractice1;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -16,9 +18,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPrivatePreferences = getSharedPreferences("practice", MODE_PRIVATE);
         // privatePreferencesに値が存在すればその値を表示をしてください。
-
+        String text = mPrivatePreferences.getString("target", "No Data");
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
         // privatePreferencesにString,int,boolean,long,floatで何らかの値を保存してください。
+        Editor editor = mPrivatePreferences.edit();
+        editor.putString("target", "hi");
+        editor.putBoolean("bool", true);
+        editor.putInt("integer", 1);
+        editor.putLong("long", 3);
+        editor.putFloat("float", (float) 0.1);
+        editor.commit();
 
         Button clearButton = (Button) findViewById(R.id.clear);
         clearButton.setOnClickListener(new OnClickListener() {
@@ -26,7 +37,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // 内容をクリアする処理を書いてください。
-
+            	mPrivatePreferences.edit().clear().commit();
             }
         });
 
